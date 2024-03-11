3149d25876f59ae46f6c22bbd188c8152197e23f
vivianbh <hadallir@gmail.com>
2024-03-11 17:16:55 +0800

    prepare done, last constraint

--- scrips/target_depth.py
+++ scrips/target_depth.py
@@ -2,10 +2,15 @@
 import rospy
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Point
+from geometry_msgs.msg import Quaternion
+from sensor_msgs.msg import JointState
 from tracking_uav_control.msg import CameraFeatures
 
 import argparse
 import numpy as np
+import math as m
+from tf.transformations import quaternion_matrix
+from utils import *
 
 parser = argparse.ArgumentParser(prog="python target_depth.py")
 parser.add_argument("-n","--uav_num", dest="uav_num", default=3, help="uav_num")
@@ -17,34 +22,108 @@ N = int(args.uav_num)
 
 class MAV():
     def __init__(self, sub_topic, ID):
-        self.pose = Point()
+        self.position = Point()
+        self.orientation = Quaternion()
         self.pose_sub = rospy.Subscriber(sub_topic, Odometry, self.pose_cb, queue_size=1)
         self.id = ID
 
     def pose_cb(self, msg):
-        self.pose = msg.pose.pose.position
+        self.position = msg.pose.pose.position
+        self.orientation = msg.pose.pose.orientation
 
-    def getPose(self):
-        return self.pose
+    def getPos(self):
+        return self.position
+    
+    def getQua(self):
+        return self.orientation
     
 class DNN():
     def __init__(self, sub_topic, ID):
+        super().__init__()
         self.bbx = CameraFeatures()
         self.detect_sub = rospy.Subscriber(sub_topic, CameraFeatures, self.detect_cb, queue_size=1)
+        self.vec = np.zeros([3,1])
+        self.id = ID
 
     def detect_cb(self, msg):
         self.bbx = msg
-
+        self.vec[0] = self.bbx.u.data - self.bbx.cu.data
+        self.vec[1] = self.bbx.v.data - self.bbx.cv.data
+        self.vec[2] = self.bbx.fx.data
+    
     def getBBX(self):
-        return self.bbx
+        return self.bbox
+    
+    def getVEC(self):
+        return self.vec
+    
+class GIMBAL():
+    def __init__(self, sub_topic, ID):
+        self.__states = JointState()
+        self.pose_sub = rospy.Subscriber(sub_topic, Odometry, self.pose_cb, queue_size=1)
+        self.state = {'roll': 0,
+                      'tilt': 0,
+                      'pan':  0,
+                      'roll_rate': 0, 
+                      'tilt_rate': 0,
+                      'pan_rate':  0}
+        self.id = ID
+
+    def pose_cb(self, msg):
+        self.__states = msg
+        self.state['roll'] = self.__states.position[0]
+        self.state['tilt'] = self.__states.position[1]
+        self.state['pan'] = self.__states.position[2]
+        self.state['roll_rate'] = self.__states.velocity[0]
+        self.state['tilt_rate'] = self.__states.velocity[1]
+        self.state['pan_rate'] = self.__states.velocity[2]
+
+    def getPos(self):
+        return self.state
 
 def DataCallback():
-    global pose_uav, info_cam
+    global pose_uav, info_cam, pose_gimbal
     pose_uav = []
     info_cam = []
+    pose_gimbal = []
+    if len(pose_uav) >= N:
+        pose_uav.clear()
+    if len(info_cam) >= N:
+        info_cam.clear()
+    if len(pose_gimbal) >= N:
+        pose_gimbal.clear()
     for i in range(N):
         pose_uav.append( MAV("/uav"+str(i)+"/base_pose_ground_truth", i) )
         info_cam.append( DNN("/uav"+str(i)+"/estimation/ukf/camera_features", i) )
+        pose_gimbal.append( GIMBAL("/uav"+str(i)+"/gimbal/joint_states", i) )
+
+def LoadingGeometry():
+    global geo_uav2pan, geo_pan2cam
+    geo_uav2pan = [] 
+    geo_pan2cam = [] 
+    for i in range(N):
+        # flu frame
+        geo_uav2pan.append(np.array([[rospy.get_param('/uav'+str(i)+'/miniyy/relPos_panPlane/x')],
+                                [rospy.get_param('/uav'+str(i)+'/miniyy/relPos_panPlane/y')],
+                                [rospy.get_param('/uav'+str(i)+'/miniyy/relPos_panPlane/z')]]))
+        # pan frame
+        geo_pan2cam.append(np.array([[rospy.get_param('/uav'+str(i)+'/miniyy/relPos_camPan/x')],
+                                [rospy.get_param('/uav'+str(i)+'/miniyy/relPos_camPan/y')],
+                                [rospy.get_param('/uav'+str(i)+'/miniyy/relPos_camPan/z')]]))
+
+def GimbalGeometryTransfer():
+    global geo_gimbal
+    geo_gimbal = []
+    for i in range(N):
+        # convert to pan frame
+        R_l2p = np.dot( R_b2p(pose_gimbal[i].getPos()['pan']), R_l2b() )
+        temp = np.dot( R_l2p, geo_uav2pan[i] ) + geo_pan2cam[i]
+        # convert to camera frame
+        temp = np.dot( R_p2c(pose_gimbal[i].getPos()['tilt']), temp )
+        # camera frame convert to gnd frame
+        geo_gimbal.append( np.dot( invR_g2c(pose_gimbal[i].getPos()['tilt'], pose_gimbal[i].getPos()['pan'], pose_uav[i].getQua()), temp))
+
+
 cnt=0
 
 class DepthEstimate():
@@ -53,15 +132,27 @@ class DepthEstimate():
         self.rate = rospy.Rate(int(args.rate))
         
         DataCallback()
+        LoadingGeometry()
 
     def estimate(self):
+        # transfer (UAV->camera) geometry into gnd frame
+        GimbalGeometryTransfer()
+
+        # camera position in gnd frame
+        pose_cam = []
+        for i in range(N):
+            pose_cam.append( np.matrix([
+                [pose_uav[i].getPos().x],
+                [pose_uav[i].getPos().y],
+                [pose_uav[i].getPos().z] ]) + geo_gimbal[i])
+            print('pose cam = ', pose_cam[i])
+
+        # construct matrices
         tau = []
         dummy = np.zeros([3,1])
         for i in range(N):
-            tau.append( np.matrix([
-                    [info_cam[i].getBBX().fx.data],
-                    [-info_cam[i].getBBX().u.data],
-                    [-info_cam[i].getBBX().v.data] ]) )
+            vec = np.dot( invR_g2c(pose_gimbal[i].getPos()['tilt'], pose_gimbal[i].getPos()['pan'], pose_uav[i].getQua()), info_cam[i].getVEC() )
+            tau.append(vec)
             print(tau[i])
             
         
@@ -71,13 +162,10 @@ class DepthEstimate():
         b = []
         for i in range(N-1):
             for j in range(i+1, N):
-                b.append( np.matrix([
-                    [pose_uav[i].getPose().x - pose_uav[j].getPose().x],
-                    [pose_uav[i].getPose().y - pose_uav[j].getPose().y],
-                    [pose_uav[i].getPose().z - pose_uav[j].getPose().z] ]) )
+                b.append( pose_cam[i] - pose_cam[j] )
                 
         B = np.vstack((b[0], b[1], b[2]))
-        print('B ',B)
+        print('B ', B)
 
         global cnt
         cnt = cnt+1
@@ -96,6 +184,10 @@ class DepthEstimate():
             d1 = np.sqrt( p1[0]**2+p1[1]**2+p1[2]**2 )
             d2 = np.sqrt( p2[0]**2+p2[1]**2+p2[2]**2 )
             d3 = np.sqrt( p3[0]**2+p3[1]**2+p3[2]**2 )
+
+            print('p1 ', p1)
+            print('p2 ', p2)
+            print('p3 ', p3)
 
             print('d1 ', d1)
             print('d2 ', d2)
---
+++ scrips/utils.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+import numpy as np
+import math as m
+from tf.transformations import quaternion_matrix
+
+def Rx(theta):
+    return np.matrix([[ 1,            0,             0],
+                        [ 0, m.cos(theta), -m.sin(theta)],
+                        [ 0, m.sin(theta),  m.cos(theta)]])
+
+def Ry(theta):
+    return np.matrix([[ m.cos(theta), 0, m.sin(theta)],
+                        [ 0           , 1,            0],
+                        [-m.sin(theta), 0, m.cos(theta)]])
+
+def Rz(theta):
+    return np.matrix([[m.cos(theta), -m.sin(theta),  0],
+                        [m.sin(theta),  m.cos(theta),  0],
+                        [           0,              0, 1]]) 
+
+# local frame convert to UAV body frame: (ENU -> FLU)
+def R_g2l(q):
+    rot_enu2flu = quaternion_matrix([q.w, q.x, q.y, q.z])
+    R = rot_enu2flu[:3, :3]
+    return R
+
+# UAV gazebo frame to UAV body frame: (FLU -> FRD)
+def R_l2b():
+    R = np.array([[1,  0,  0],
+                  [0, -1,  0],
+                  [0,  0, -1]])
+    return R
+
+# local frame convert to UAV body frame: (ENU -> FRD)
+def R_g2b(q):
+    R = np.dot(R_l2b(), R_g2l(q))
+    return R
+
+# UAV body frame convert to gimbal pan frame
+def R_b2p(ang):
+    # [units]: radian
+    R = Rz(ang)
+    return R
+
+# gimbal tilt frame convert to camera frame
+def R_p2c(ang):
+    # [units]: radian
+    rot_pan2tilt = Ry(ang)
+    rot_tilt2cam = np.dot( Rx(m.pi/2), Rz(m.pi/2) )
+    R = np.dot(rot_tilt2cam, rot_pan2tilt)
+    return R
+
+def R_g2c(tilt, pan, qua):
+    return np.dot( R_p2c(tilt), R_b2p(pan), R_g2b(qua) )
+
+def invR_g2c(tilt, pan, qua):
+    return np.linalg.inv( R_g2c(tilt, pan, qua) )
